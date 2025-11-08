3bc470eec8ac77e63e87efe34d31a707adb4cf9e
leosrabello <122031549+leosrabello@users.noreply.github.com>
2025-11-08 12:37:27 -0300

    Commit Questão 1

--- Q1/AlgoritmoPerdaEsperada.java
+++ Q1/AlgoritmoPerdaEsperada.java
@@ -4,6 +4,7 @@ import java.math.BigDecimal;
 import java.math.MathContext;
 import java.math.RoundingMode;
 
+
 public final class AlgoritmoPerdaEsperada implements AlgoritmoRisco {
     private static final MathContext MC = new MathContext(10, RoundingMode.HALF_UP);
 
